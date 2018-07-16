ef1f7f06241d884cb1763910d53051b2cde1ba3b
Tirumarai Selvan <tiru@hasura.io>
2018-07-16 20:50:18 +0530

    deploy order and analytics apps

--- heroku-setup.sh
+++ heroku-setup.sh
@@ -6,17 +6,27 @@ cd temp
 rm -rf graphql-engine-heroku
 git clone https://github.com/hasura/graphql-engine-heroku.git
 cd graphql-engine-heroku
-heroku apps:create
+HASURA_APP_INFO=$(heroku apps:create --remote hasura --json)
 heroku stack:set container
-INFO=$(heroku apps:info --json)
-NAME=$(echo $INFO | jq -r ".app.name")
-HTTP_URL=$(echo $INFO | jq -r ".app.web_url")
-heroku addons:create heroku-postgresql:hobby-dev -a $NAME
-git push heroku master
+HASURA_APP_NAME=$(echo $HASURA_APP_INFO | jq -r ".name")
+HASURA_APP_WEB_URL=$(echo $HASURA_APP_INFO | jq -r ".web_url")
+heroku addons:create heroku-postgresql:hobby-dev -a $HASURA_APP_NAME
+git push hasura master
 
-echo "endpoint: $HTTP_URL" > ../../hasura/config.yaml
+echo "endpoint: $HASURA_APP_WEB_URL" > ../../hasura/config.yaml
 cd ../../hasura
 hasura migrate apply
+
+cd ..
+
+ORDER_APP_INFO=$(heroku apps:create --remote orders --json)
+ORDER_APP_WEB_URL=$(echo $ORDER_APP_INFO | jq -r ".web_url")
+git subtree push --prefix order-app orders master
+
+
+ANALYTICS_APP_INFO=$(heroku apps:create --remote analytics --json)
+ANALYTICS_APP_WEB_URL=$(echo $ANALYTICS_APP_INFO | jq -r ".web_url")
+git subtree push --prefix analytics analytics master
 
 
 GREEN='\033[0;32m'
@@ -26,4 +36,10 @@ echo
 echo -e "${GREEN}Run \`hasura console\` from the \`hasura\` directory${NC}"
 echo
 
+echo
+echo -e "${GREEN}Order app is running at $ORDER_APP_WEB_URL${NC}"
+echo
 
+echo
+echo -e "${GREEN}Analytics app is running at $ANALYTICS_APP_WEB_URL${NC}"
+echo
