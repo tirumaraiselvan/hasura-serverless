d00f7cd5faceadfc8eb11950934bfb713efc2426
Tanmai Gopal <tanmaig@34cross.in>
2018-07-25 11:08:19 +0530

    adds steps

--- order-app/src/GetStatus.js
+++ order-app/src/GetStatus.js
@@ -1,38 +1,74 @@
 import React from 'react';
-import {Badge} from 'react-bootstrap';
+import {Glyphicon, Badge} from 'react-bootstrap';
 
-// const icons = {
-//   running: <Glyphicon glyph="flash" />,
-//   waiting: <Glyphicon glyph="warning-sign" />,
-//   done: <Glyphicon glyph="tick" />,
-//   notyet: <Glyphicon glyph="more" />,
-// };
-// const statuses = [
-//   {
-//     name: 'Order validation'
-//   },
-//   {
-//     name: 'Payment'
-//   },
-//   {
-//     name: 'Restaurant '
-//   },
-//   {
-//     name: 'Driver assigment'
-//   },
-// ];
+const icons = {
+  running: (<Glyphicon glyph="flash" />),
+  waiting: (<Glyphicon glyph="flash" />),
+  done:    (<Glyphicon glyph="ok" />),
+  notyet:  (<Glyphicon glyph="remove" />)
+};
+
+const statuses = [
+  {
+    name: 'Order validation',
+    value: 'notyet'
+  },
+  {
+    name: 'Your payment',
+    value: 'notyet'
+  },
+  {
+    name: 'Restaurant approval',
+    value: 'notyet'
+  },
+  {
+    name: 'Driver assignment',
+    value: 'notyet'
+  }
+];
+
+const createBar = (statusBar) => {
+  return (
+    <div>
+      {statusBar.map((s,i) => {
+        return (
+          <span key={i}>
+            {icons[s.value]} -
+            {s.value === 'running' ? (<b style={{color: 'green'}}>{s.name}</b>) : s.name}
+            <br/>
+          </span>
+        );
+      })}
+    </div>
+  );
+};
+
 export default ({order_valid, payment_valid, approved, driver_assigned}) => {
-  // const statusBar = [...statuses];
+  const statusBar = JSON.parse(JSON.stringify(statuses));
   if (!(order_valid)) {
-    return (<span><Badge>1</Badge> 'Processing order...'</span>);
+    statusBar[0].value = 'running';
+    return createBar(statusBar);
   } else if (order_valid && !payment_valid) {
-    return (<span><Badge>2</Badge> 'Waiting for your payment...'</span>);
+    statusBar[0].value = 'done';
+    statusBar[1].value = 'running';
+    return createBar(statusBar);
   } else if (payment_valid && !approved) {
-    return (<span><Badge>3</Badge> 'Waiting for restaurant...'</span>);
+    statusBar[0].value ='done';
+    statusBar[1].value ='done';
+    statusBar[2].value ='running';
+    return createBar(statusBar);
   } else if (approved && !driver_assigned) {
-    return (<span><Badge>4</Badge> 'Assigning driver...'</span>);
+    statusBar[0].value ='done';
+    statusBar[1].value ='done';
+    statusBar[2].value ='done';
+    statusBar[3].value ='running';
+    return createBar(statusBar);
   } else if (driver_assigned) {
-    return (<span><Badge>5</Badge> 'Driver assigned!'</span>);
+    statusBar[0].value ='done';
+    statusBar[1].value ='done';
+    statusBar[2].value ='done';
+    statusBar[3].value ='done';
+    return createBar(statusBar);
   } else {
     return (<span><Badge>!?!</Badge> 'Unknown state'</span>);
   }
